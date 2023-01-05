11a556fded2a494c9c70288c7c9a1d8f072a023b
Maksim Matveev <thematveev21@gmail.com>
2023-01-05 22:05:07 +0200

    some improvments

---
+++ config.py
@@ -0,0 +1,10 @@
+# rapid api weather keys
+weather_rapid_api_key = "b1ea0a93b5mshb2b8e78b045f449p11b71cjsn6d6517db9378"
+# flask config
+flask_secret_key = "supersecretkey"
+HOST = "127.0.0.1"
+PORT = 5000
+
+
+# database
+DATABASE_NAME = "database.db"
